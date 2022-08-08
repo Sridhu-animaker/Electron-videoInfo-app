9a02315d84c388784d12a6392f02b346ca1f163f
Sridhu-animaker <sridhu@animaker.com>
2022-08-08 11:28:42 +0530

    Coments added

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ const { app, BrowserWindow, ipcMain } = electron;
 
 const ffmpeg = require("fluent-ffmpeg");
 let mainWindow;
+// The below method will be called initially.
 app.on("ready", () => {
-  console.log("Electron app is ready.....");
+
   mainWindow = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: true, // This should be true to aviod error while compiling.
       contextIsolation: false,
     },
   });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
+// On video submit we will recieve all the details in ipcMain (ffmpeg is used to get all the data)
 ipcMain.on("video:submit", (event, path) => {
   ffmpeg.ffprobe(path, (err, metaData) => {
-    mainWindow.webContents.send("video:metadata", metaData.format.duration);
-    console.log("Vodeo duration:", metaData.format.duration);
+    mainWindow.webContents.send("video:metadata", metaData.format);
   });
 });
--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const { ipcRenderer } = electron;
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
   const { path } = document.querySelector("input").files[0];
+  // ipcRenderer will send the perticular event
   ipcRenderer.send("video:submit", path);
 });
-
-ipcRenderer.on("video:metadata", (event, duration)=>{
-    document.querySelector("#result").innerHTML = `Video duration is: ${duration} seconds`
-})
-
+// ipcRenderer will will recieve the event
+ipcRenderer.on("video:metadata", (event, format) => {
+  let filePath = format.filename.split("/");
+  let fileName = filePath[filePath.length - 1];
+  document.querySelector("#fileName").innerHTML = `File Name is: ${fileName}`;
+  document.querySelector(
+    "#result"
+  ).innerHTML = `Video duration is: ${format.duration} seconds`;
+});
